d69327891d737711057f3cd05168b0dc742bb726
michellemetni <michellemetni@gmail.com>
2024-11-03 22:31:31 +0200

    Add new files

---
+++ DecoratorPattern/StarbuzzCoffee.java
@@ -0,0 +1,23 @@
+public class StarbuzzCoffee {
+    public static void main(String[] args) {
+        Beverage beverage1 = new Espresso();
+        System.out.println(beverage1.getDescription() + " $" + beverage1.cost());
+
+        Beverage beverage2 = new DarkRoast();
+        beverage2 = new Mocha(beverage2);
+        beverage2 = new Mocha(beverage2);
+        beverage2 = new Whip(beverage2);
+        System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+
+        Beverage beverage3 = new HouseBlend();
+        beverage3 = new Mocha(beverage3);
+        beverage3 = new Whip(beverage3);
+        System.out.println(beverage3.getDescription() + " $" + beverage3.cost());
+
+        // New beverage with a new decorator
+        Beverage beverage4 = new Decaf();
+        beverage4 = new SteamedMilk(beverage4);
+        beverage4 = new Mocha(beverage4);
+        System.out.println(beverage4.getDescription() + " $" + beverage4.cost());
+    }
+}
